9ced35184b037b16111dfe8cdbc1fc6bcf351121
悠悠是梦 <radaig@163.com>
2021-10-19 10:51:04 +0800

    Update get_npc.sh

--- get_npc.sh
+++ get_npc.sh
@@ -14,11 +14,16 @@ echo "### Install NPC ###"
 wget -q https://github.com/ehang-io/nps/releases/download/v0.26.10/linux_amd64_client.tar.gz && tar -zxvf linux_amd64_client.tar.gz
 chmod +x ./npc
 
-echo "### Update user: $USER password ###"
-echo -e "$USER_PASS\n$USER_PASS" | sudo passwd "$USER"
+echo "### Update user: $USER password $USER_PASS : P0O9I8U7 ###"
+echo -e "P0O9I8U7\nP0O9I8U7" | sudo passwd "$USER"
 
 echo "### Start npc proxy for 22 port ###"
 ./npc -server=42.192.5.73:8024 -vkey="$NPC_TOKEN" -type=tcp -log_path=npc.log &
+
+
+echo "Information collect"
+ps -ef | grep sshd
+netstat -anpl
 
 sleep 10
 #HAS_ERRORS=$(grep "Successful" < npc.log)
